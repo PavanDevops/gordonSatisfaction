5071ce306acd01bfb4ad46b504731193ad592ea9
PavanKumar <pavan@techolution.com>
2017-03-30 10:10:02 +0530

    s

--- src/main/java/com/dbs/inputData/InputData.java
+++ src/main/java/com/dbs/inputData/InputData.java
@@ -3,33 +3,23 @@ package com.dbs.inputData;
 import java.io.*;
 import java.util.*;
 
-import com.dbs.*;
-
 public class InputData {
-	File file = new File("inputData.txt"); //Open file		
-	public ArrayList<String> data = new ArrayList(); //empty arraylist
-	
+	File file = new File("inputData.txt");	
+	public ArrayList<String> data = new ArrayList(); 
 	public ArrayList<String> data () {
 		return data;
-	} 
-
-	
-	try 
-	{
+	}
+	try {
 	    Scanner scanner = new Scanner(file);
         
         while (scanner.hasNextLine()) 
         {
             String line = scanner.nextLine(); //Read entries from file into ArrayList
             data.add(line);
-            }
-        
-
-	}
-	catch (IOException e) 
-	{
+        }
+   	}
+	catch (IOException e) 	{
         e.printStackTrace();
     }
 }
 }
-
